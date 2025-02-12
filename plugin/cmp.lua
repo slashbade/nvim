abf26b4c8a6b112d8c307f531f28febfb9132a9a
slashbade <slashbladevitali@gmail.com>
2025-02-13 00:13:34 +0800

    update cmdline cmp

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -16,7 +16,30 @@ cmp.setup{
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'path' },
+      { name = '' },
       { name = 'buffer' },
       { name = 'copilot' },
     })
 }
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+    { name = 'buffer' }
+    }
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+      ignore_cmds = { 'Man', '!' }
+    }
+  }
+})
+})
